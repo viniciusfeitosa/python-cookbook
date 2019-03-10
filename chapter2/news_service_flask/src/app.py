b4763ff55eaff28ed70a9a3cbc07172e4fcf057f
Vinicius Pacheco <vfpweb@gmail.com>
2019-03-10 02:17:29 -0300

    Update flaks project to use flask migrate

--- chapter2/news_service_flask/src/app.py
+++ chapter2/news_service_flask/src/app.py
@@ -1,6 +1,7 @@
 # src/app.py
 
 from flask import Flask
+from flask_migrate import Migrate
 
 from .config import app_config
 from .models import db
@@ -14,6 +15,7 @@ def load_app(env_name):
     app.config.from_object(app_config[env_name])
 
     db.init_app(app)
+    Migrate(app, db)
 
     app.register_blueprint(news_blueprint, url_prefix='/news')
 
